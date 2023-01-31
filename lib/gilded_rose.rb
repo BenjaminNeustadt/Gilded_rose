e665dc62aaf887c229680ce41baf5861046aee4b
benjamin <b.james.neustadt@gmail.com>
2023-01-31 15:33:24 +0100

    Extract distinct functionality into separate methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,18 +9,27 @@ class GildedRose
     @items = items
   end
 
+  def standard_decrease(item)
+    item.quality = item.quality - 1
+    item.sell_in = item.sell_in - 1
+  end
+
+  def perished_decrease(item)
+    item.quality = item.quality - 2
+    item.sell_in = item.sell_in - 1
+  end
+
   def update_quality()
     @items.each do |item|
       if item.sell_in > 0
         unless item.quality == 0
-          item.quality = item.quality - 1
-          item.sell_in = item.sell_in - 1
+          standard_decrease(item)
         end
       else
-          item.quality = item.quality - 2
-          item.sell_in = item.sell_in - 1
+          perished_decrease(item)
       end
     end
+
   end
   #     if item.name != PRODUCT[:cheese] and item.name != PRODUCT[:ticket]
 
